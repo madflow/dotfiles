ad1216c9aa303adbc1e1796bb419749b2ef5d9b3
madflow <madflow@frikadelic.net>
2023-07-18 18:44:09 +0200

    🤗

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-RUN_TIME=`date +"%Y-%m-%d-%H-%M-%S"`
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+RUN_TIME=$(date +"%Y-%m-%d-%H-%M-%S")
 # POSIX: https://pubs.opengroup.org/onlinepubs/009696899/basedefs/xbd_chap08.html#tag_08_03
 HOME="${HOME:-$(getent passwd $USER 2>/dev/null | cut -d: -f6)}"
 # macOS does not have getent, but this works even if $HOME is unset
@@ -12,28 +12,28 @@ OMYZSH="${HOME}/.oh-my-zsh"
 
 function prompt_install() {
 
-    if ! [ -x "$(command -v ${1})" ]; then
-        inst=${3-$1}
-        echo "${1} cannot be found. Try installing it with:" >&2
-        if [ -x "$(command -v pacman)" ]; then
-            echo "pacman -S ${inst}" >&2
-        fi
-        if [ -x "$(command -v apt-get)" ]; then
-            echo "apt-get install ${inst}" >&2
-        fi
-        if [ -x "$(command -v brew)" ]; then
-            echo "brew install ${inst}" >&2
-        fi
-        if [ -x "$(command -v apk)" ]; then
-            echo "apk add ${inst}" >&2
-        fi
-        if [ -z ${2+x} ]; then
-            echo "No installation instructions provided" >&2
-        else
-            echo "Installation instructions can be found at: ${2}"  >&2
-        fi
-        exit 1
-    fi
+	if ! [ -x "$(command -v ${1})" ]; then
+		inst=${3-$1}
+		echo "${1} cannot be found. Try installing it with:" >&2
+		if [ -x "$(command -v pacman)" ]; then
+			echo "pacman -S ${inst}" >&2
+		fi
+		if [ -x "$(command -v apt-get)" ]; then
+			echo "apt-get install ${inst}" >&2
+		fi
+		if [ -x "$(command -v brew)" ]; then
+			echo "brew install ${inst}" >&2
+		fi
+		if [ -x "$(command -v apk)" ]; then
+			echo "apk add ${inst}" >&2
+		fi
+		if [ -z ${2+x} ]; then
+			echo "No installation instructions provided" >&2
+		else
+			echo "Installation instructions can be found at: ${2}" >&2
+		fi
+		exit 1
+	fi
 }
 
 prompt_install 'curl'
@@ -44,32 +44,29 @@ prompt_install 'wget'
 prompt_install 'zsh'
 prompt_install 'fzf'
 prompt_install 'php'
+prompt_install 'neovim'
 
-
-if [[ -d "$OMYZSH" ]]
-then
-    echo "oh my zsh seems to already installed."
+if [[ -d "$OMYZSH" ]]; then
+	echo "oh my zsh seems to already installed."
 else
-    echo "Installing oh my zsh. Restart this script after oh my zsh is installed. Sleeping for 5 seconds..."
-    sleep 5s
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+	echo "Installing oh my zsh. Restart this script after oh my zsh is installed. Sleeping for 5 seconds..."
+	sleep 5s
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
 # Clone the repo if needed
-if [ -d ~/.dotfiles ]
-then
-    echo ".dotfiles exists"
+if [ -d ~/.dotfiles ]; then
+	echo ".dotfiles exists"
 else
-    git clone https://github.com/madflow/dotfiles.git ~/.dotfiles
+	git clone https://github.com/madflow/dotfiles.git ~/.dotfiles
 fi
 
 # Vim
-if [ -d ~/.vim/bundle/Vundle.vim ]
-then
-    echo "Vundle directory exists. Updating..."
-    cd ~/.vim/bundle/Vundle.vim && git pull
+if [ -d ~/.vim/bundle/Vundle.vim ]; then
+	echo "Vundle directory exists. Updating..."
+	cd ~/.vim/bundle/Vundle.vim && git pull
 else
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
@@ -79,13 +76,15 @@ vim +PluginInstall +qall
 # Neovim
 ln -sf ~/.dotfiles/nvim ~/.config/nvim
 
+# Wezterm
+ln -sf ~/.dotfiles/wezterm ~/.config/wezterm
+
 # Tmux
-if [ -d ~/.tmux/plugins/tpm ]
-then
-    echo "Tpm directory exists. Updating..."
-    cd ~/.tmux/plugins/tpm && git pull
+if [ -d ~/.tmux/plugins/tpm ]; then
+	echo "Tpm directory exists. Updating..."
+	cd ~/.tmux/plugins/tpm && git pull
 else
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 ln -sf $SCRIPT_DIR/tmux.conf ~/.tmux.conf
 # Install and update TPM plugins
@@ -95,20 +94,17 @@ ln -sf $SCRIPT_DIR/tmux.conf ~/.tmux.conf
 # Git
 touch ~/.gitconfig.local
 
-if [ -L ~/.gitconfig ]
-then
-    echo "Git config does already exists as a symlink."
+if [ -L ~/.gitconfig ]; then
+	echo "Git config does already exists as a symlink."
 else
-    if [ -f ~/.gitconfig ]
-    then
-        echo "Git config already exists. Backing up"
-        mv ~/.gitconfig ~/.gitconfig.$RUN_TIME
-    else
-        echo "Git config does not already exist. Creating it."
-    fi
+	if [ -f ~/.gitconfig ]; then
+		echo "Git config already exists. Backing up"
+		mv ~/.gitconfig ~/.gitconfig.$RUN_TIME
+	else
+		echo "Git config does not already exist. Creating it."
+	fi
 fi
 ln -sf $SCRIPT_DIR/gitconfig ~/.gitconfig
-
 
 # ZSH
 ln -sf $SCRIPT_DIR/zshrc ~/.zshrc
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,48 @@
+local wezterm = require("wezterm")
+
+local function font(opts)
+	return wezterm.font_with_fallback({
+		opts,
+		"Symbols Nerd Font Mono",
+	})
+end
+return {
+	audible_bell = "Disabled",
+	bold_brightens_ansi_colors = true,
+	color_scheme = "tokyonight_moon",
+	color_scheme_dirs = { "~/.config/wezterm/themes" },
+	font = font({ family = "Hack", weight = "Bold", italic = false }),
+	allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace",
+	font_size = 16,
+	hide_tab_bar_if_only_one_tab = true,
+	native_macos_fullscreen_mode = true,
+	scrollback_lines = 10000,
+	show_tab_index_in_tab_bar = false,
+	term = "wezterm",
+	use_fancy_tab_bar = true,
+	window_background_opacity = 1.0,
+	send_composed_key_when_left_alt_is_pressed = true,
+	send_composed_key_when_right_alt_is_pressed = false,
+	window_frame = {
+		-- The font used in the tab bar.
+		-- Roboto Bold is the default; this font is bundled
+		-- with wezterm.
+		-- Whatever font is selected here, it will have the
+		-- main font setting appended to it to pick up any
+		-- fallback fonts you may have used there.
+		font = font({ family = "Hack", weight = "Bold" }),
+
+		-- The size of the font in the tab bar.
+		-- Default to 10. on Windows but 12.0 on other systems
+		font_size = 16.0,
+
+		-- The overall background color of the tab bar when
+		-- the window is focused
+		active_titlebar_bg = "#12131d",
+
+		-- The overall background color of the tab bar when
+		-- the window is not focused
+		inactive_titlebar_bg = "#1e2030",
+	},
+	hyperlink_rules = wezterm.default_hyperlink_rules(),
+}
